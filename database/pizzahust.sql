558763ccf31083713768f582a2f1e8232563619f
dnh1704 <56498669+dnh1704@users.noreply.github.com>
2021-10-30 20:54:01 +0700

    database + form login

---
+++ database/pizzahust.sql
@@ -0,0 +1,67 @@
+CREATE TABLE `admin` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(50),
+  `user_name` varchar(50),
+  `password` varchar(30)
+);
+
+CREATE TABLE `user` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(50),
+  `address` varchar(100),
+  `phone_number` varchar(13)
+);
+
+CREATE TABLE `category` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(100)
+);
+
+CREATE TABLE `product` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `category_id` int,
+  `title` varchar(350),
+  `price` int,
+  `weight` int,
+  `status` int,
+  `date` datetime,
+  `thumbnail` varchar(500),
+  `description` varchar(255)
+);
+
+CREATE TABLE `galery` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `product_id` int,
+  `thumbnail` varchar(500)
+);
+
+CREATE TABLE `order` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int,
+  `fullname` varchar(50),
+  `phone_number` varchar(13),
+  `address` varchar(100),
+  `note` varchar(255),
+  `order_date` datetime,
+  `status` int,
+  `total_money` int
+);
+
+CREATE TABLE `order_detail` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `order_id` int,
+  `product_id` int,
+  `price` int,
+  `num` int,
+  `total_money` int
+);
+
+ALTER TABLE `product` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+
+ALTER TABLE `galery` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+ALTER TABLE `order_detail` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+ALTER TABLE `order_detail` ADD FOREIGN KEY (`order_id`) REFERENCES `order` (`id`);
+
+ALTER TABLE `order` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);

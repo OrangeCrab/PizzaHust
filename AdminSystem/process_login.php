1d7ec43808232b3e67f72e8d12596f1622b37ab8
dnh1704 <huanhuanhuank56a1@gmail.com>
2021-11-03 01:15:38 +0700

    them comment vao phan login va phan editor them sp

--- AdminSystem/process_login.php
+++ AdminSystem/process_login.php
@@ -1,24 +1,27 @@
 <?php 
 
 $password = $user_name= $msg = '';
+    // kiem tra xem nguoi dung co nhap du lieu khong
+    // bằng cách kiem tra xem co qua trinh POST len server khong
+   
     if(!empty($_POST)){
+        //Neu co
         $user_name = getPost('username');
         $password = getPost('password');
-
+        
+        // chay cau lenh sql de lay ra gia tri trong database co gia tri bang gia tri nguoi dung nhap vao
         $sql = "select * from admin where user_name = '$user_name' and password = '$password'";
         $userExist = executeResult($sql);
+
+
+        // neu khong co nguoi dung nao
         if($userExist == null){
             $msg = "Đăng nhập không thành công, vui lòng kiểm tra lại tên đăng nhập hoặc mật khẩu";
            // dang nhap khong thanh cong 
         }
         else{
             // dang nhap thanh cong
-<<<<<<< HEAD
-            //echo"thành công";
             header('location: Dashboard/DashBoard.html');
-=======
-            header('location: DashBoard/DashBoard.html');
->>>>>>> 7df2f95c3dff55958f686a93b165c627bbd927c8
             die();
         }   
      }

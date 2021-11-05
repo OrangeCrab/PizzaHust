ed46d1325c6b2859225b58cccb50ff912452dabf
PhamVanDiep <phamdiepa1k55@gmail.com>
2021-11-05 16:52:54 +0700

    update giao diện của popup

--- AdminSystem/CodeProductManagementPage/ProductManagementPage.php
+++ AdminSystem/CodeProductManagementPage/ProductManagementPage.php
@@ -10,7 +10,7 @@
     $title = 'Thêm/Sửa Sản Phẩm';
 	$baseUrl = '../';
 
-	$id = $msg = $title = $price = $weight = $status_id = $category_id = $date = $thumbnail = $description='';
+	$id = $msg = $title = $price  = $status_id = $category_id = $date = $thumbnail = $description='';
 
     require_once('../../database/utility.php');
 	require_once('form_save.php');
@@ -37,7 +37,6 @@
 			$category_id = getArrResult($sql)['category_id'];
 			$price = getArrResult($sql)['price'];
 			$title = getArrResult($sql)['title'];
-			$weight = getArrResult($sql)['weight'];
 			$status_id = getArrResult($sql)['status_id'];
 			$description = getArrResult($sql)['description'];
             $thumbnail = getArrResult($sql)['thumbnail'];
@@ -93,7 +92,6 @@
         <div class="work_screen">
             <div class="left_bar scroll">
                 <a href="../DashBoard/DashBoard.html"><i class="fa fa-tachometer" aria-hidden="true"></i><span>Tổng Quan</span></a>
-                <a href="../CategoryManagementPage/CategoryManagement.php"><i class="fa fa-cutlery" aria-hidden="true"></i><span>Danh Mục Món Ăn</span></a>
                 <a href="#" class="active"><i class="fa fa-cutlery" aria-hidden="true"></i><span>Món Ăn</span></a>
                 <a href="../InvoiceManagementPage/InvoiceManagementPage.html"><i class="fa fa-cube" aria-hidden="true"></i><span>Đơn Hàng</span></a>
             </div>
@@ -106,7 +104,6 @@
                         <input type="text" placeholder="Tìm kiếm" id="search_tf"/>
                     </div>
                     <div class="wrap_btn">
-                        <!-- <a href="#editProduct_popup"></a> -->
                         <button class="btn btn-success add_button">Thêm sản phẩm</button>
                     </div>
                     
@@ -160,61 +157,62 @@
                 <h3>Thêm sản phẩm</h3>
                 <div class="panel-body">
                     <form method="post" enctype="multipart/form-data">
-                        <div class="form_group" style="top: 5%">
-                            <label for="usr">Tên sản phẩm:</label>
-                            <input required="true" type="text" class="form-control" id="usr" name="title" value="<?=$title?>">
-                            <input type="text" name="id" value="<?=$id?>" hidden="true">
-                        </div>
-                        
-                        <div class="form_group" style="top: 15%">
-                            <label for="usr">Loại Sản Phẩm:</label>
-                            <select class="form-control" name="category_id" id="category_id" required="true">
-                                <option value="">-- Chọn --</option>
-                                <?php
-                                    foreach($categoryItems as $category) {
-                                        if($category['id'] == $category_id) {
-                                            echo '<option selected value="'.$category['id'].'">'.$category['name'].'</option>';
-                                        } else {
-                                            echo '<option value="'.$category['id'].'">'.$category['name'].'</option>';
+                        <div class="left_div">
+                            <div class="form_group" style="top: 5%">
+                                <label for="usr">Tên sản phẩm:</label>
+                                <input required="true" type="text" class="form-control" id="usr" name="title" value="<?=$title?>">
+                                <input type="text" name="id" value="<?=$id?>" hidden="true">
+                            </div>
+                            
+                            <div class="form_group" style="top: 20%">
+                                <label for="usr">Loại Sản Phẩm:</label>
+                                <select class="form-control" name="category_id" id="category_id" required="true">
+                                    <option value="">-- Chọn --</option>
+                                    <?php
+                                        foreach($categoryItems as $category) {
+                                            if($category['id'] == $category_id) {
+                                                echo '<option selected value="'.$category['id'].'">'.$category['name'].'</option>';
+                                            } else {
+                                                echo '<option value="'.$category['id'].'">'.$category['name'].'</option>';
+                                            }
                                         }
-                                    }
-                                ?>
-                            </select>
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="form_group" style="top: 35%">
+                                <label for="description">Mô tả sản phẩm:</label>
+                                <input required="true" type="text" class="form-control" id="description" name="description" value="<?=$description?>">
+                            </div>
+                            
+                            <div class="form_group"style="top: 50%">
+                                <label for="status">Trạng thái:</label>
+                                <select class="form-control" name="status_id" id="status_id" required="true">
+                                    <option value="">-- Chọn --</option>
+                                    <?php
+                                        foreach($statusList as $status) {
+                                            if($status['id'] == $status_id) {
+                                                echo '<option selected value="'.$status['id'].'">'.$status['status'].'</option>';
+                                            } else {
+                                                echo '<option value="'.$status['id'].'">'.$status['status'].'</option>';
+                                            }
+                                        }
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="form_group" style="top: 65%">
+                                <label for="price">Giá:</label>
+                                <input required="true" type="number" class="form-control" id="price" name="price" value="<?=$price?>">
+                            </div>
                         </div>
-                        <div class="form_group" style="top: 25%">
-                            <label for="description">Mô tả sản phẩm:</label>
-                            <input required="true" type="text" class="form-control" id="description" name="description" value="<?=$description?>">
+                        <div class="right_div">
+                            <div class="title_of_img_card">
+                                <label for="thumbnail">Hình ảnh sản phẩm:</label>
+                            </div>
+                            <div class="img_card">
+                                <input type="file" name="image" id="product_img">
+                            </div>
                         </div>
-                        
-                        <div class="form_group"style="top: 35%">
-                            <label for="status">Trạng thái:</label>
-                            <select class="form-control" name="status_id" id="status_id" required="true">
-								<option value="">-- Chọn --</option>
-								<?php
-									foreach($statusList as $status) {
-										if($status['id'] == $status_id) {
-											echo '<option selected value="'.$status['id'].'">'.$status['status'].'</option>';
-										} else {
-											echo '<option value="'.$status['id'].'">'.$status['status'].'</option>';
-										}
-									}
-								?>
-							</select>
-                        </div>
-                        <div class="form_group" style="top: 45%">
-                            <label for="price">Giá:</label>
-                            <input required="true" type="number" class="form-control" id="price" name="price" value="<?=$price?>">
-                        </div>
-                        
-                        <div class="form_group" style="top: 55%">
-                            <label for="weight">Khối lượng:</label>
-                            <input required="true" type="number" class="form-control" id="weight" name="weight" value="<?=$weight?>">
-                        </div>
-                        <div class="form_group" style="top: 65%">
-                            <label for="thumbnail">Hình ảnh sản phẩm:</label>
-                            <input required="true" type="text" class="form-control" id="thumbnail" name="thumbnail" value="<?=$thumbnail?>">
-                        </div>     
-                        <div class="cfbtn_div" style="top: 85%">
+                        <div class="bottom_div">
                             <button class="btn btn-success">Xác Nhận</button>
                         </div>
                     </form>
--- AdminSystem/CodeProductManagementPage/editProductPopup.php
+++ AdminSystem/CodeProductManagementPage/editProductPopup.php
@@ -2,7 +2,7 @@
     $title = 'Sửa Sản Phẩm';
 	$baseUrl = '../';
 
-	$id = $msg = $title = $price = $weight = $status_id = $category_id = $date = $thumbnail = $description='';
+	$id = $msg = $title = $price =  $status_id = $category_id = $date = $thumbnail = $description='';
 
     require_once('../../database/utility.php');
     require_once('../../database/define.php');
@@ -12,11 +12,10 @@
 	$id = getGet('id');
 
     $sql = "select * from product where id = '$id' ";
-    
+
     $category_id = getArrResult($sql)['category_id'];
     $price = getArrResult($sql)['price'];
     $title = getArrResult($sql)['title'];
-    $weight = getArrResult($sql)['weight'];
     $status_id = getArrResult($sql)['status_id'];
     $description = getArrResult($sql)['description'];
     $thumbnail = getArrResult($sql)['thumbnail'];
@@ -69,19 +68,20 @@
                 background: #f2f2f2;
                 border-radius: 5px;
             }
-            .product_popup .info_card .close_btn{
-                color: #404040;
-                z-index: 3;
-                position: absolute;
-                right: 0;
-                font-size: 20px;
-                margin: 20px;
-                cursor: pointer;
-            }
+                .product_popup .info_card .close_btn{
+                    color: #404040;
+                    z-index: 3;
+                    position: absolute;
+                    right: 0;
+                    font-size: 20px;
+                    margin: 20px;
+                    cursor: pointer;
+                }
                 .product_popup .info_card h3{
                     position: absolute;
-                    left: 40%;
-                    height: 10%;
+                    top: 5%;
+                    left: 35%;
+                    height: 5%;
                 }
                 .panel-body{
                     position: absolute;
@@ -94,102 +94,133 @@
                         width: 100%;
                         height: 100%;
                     }
-
-                    .form_group{
-                        position: absolute;
-                        width: 100%;
-                        height: 8%;
-                    }
-                    .form_group label{
-                        height: 60%;
-                        width: 20%;
-                        text-align: right;
-                        clear: both;
-                        float:left;
-                        margin-right:5%;
-                        margin-top: 0.5%;
-                    }
-                    .form_group input{
-                        position: absolute;
-                        width: 70%;
-                        height: 100%;
-                        left: 25%;
-                    }
-                    .form_group select{
-                        position: absolute;
-                        width: 70%;
-                        height: 100%;
-                        left: 25%;
-                    }
-                    .cfbtn_div{
-                        position: absolute;
-                        width: 20%;
-                        height: 10%;
-                        left: 45%;
-                    }
+                        .left_div{
+                            position: absolute;
+                            width: 50%;
+                            height: 80%;
+                            top: 15%;
+                        }
+                            .form_group{
+                                position: absolute;
+                                width: 100%;
+                                height: 10%;
+                            }
+                            .form_group label{
+                                height: 60%;
+                                width: 35%;
+                                text-align: right;
+                                clear: both;
+                                float:left;
+                                margin-right:5%;
+                                margin-top: 0.5%;
+                            }
+                            .form_group input{
+                                position: absolute;
+                                width: 50%;
+                                height: 100%;
+                                left: 40%;
+                            }
+                            .form_group select{
+                                position: absolute;
+                                width: 50%;
+                                height: 100%;
+                                left: 40%;
+                            }
+                        .right_div{
+                            position: absolute;
+                            width: 50%;
+                            height: 80%;
+                            top: 10%;
+                            left: 50%;
+                        }
+                            .title_of_img_card{
+                                position: absolute;
+                                width: 100%;
+                                height: 10%;
+                                top: 0;
+                                text-align: center;
+                            }
+                            .img_card{
+                                position: absolute;
+                                width: 80%;
+                                height: 75%;
+                                top: 10%;
+                                left: 10%;
+                                text-align: center;
+                                border: 1px  solid  #000000;
+                            }
+                        .bottom_div{
+                            position: absolute;
+                            width: 20%;
+                            height: 10%;
+                            left: 45%;
+                            top: 85%;
+                        }
         </style>
     </head>
     <body>
-        <div class="product_popup">
+        <div class="product_popup" id="editProduct_popup">
             <div class="info_card">
-                <h3>Thêm sản phẩm</h3>
+                <a href="ProductManagementPage.php"><i class="fa fa-times close_btn" aria-hidden="true"></i></a>
+                <h3>Sửa thông tin sản phẩm</h3>
                 <div class="panel-body">
                     <form method="post" enctype="multipart/form-data">
-                        <div class="form_group" style="top: 5%">
-                            <label for="usr">Tên sản phẩm:</label>
-                            <input required="true" type="text" class="form-control" id="usr" name="title" value="<?=$title?>">
-                            <input type="text" name="id" value="<?=$id?>" hidden="true">
-                        </div>
-                        
-                        <div class="form_group" style="top: 15%">
-                            <label for="usr">Loại Sản Phẩm:</label>
-                            <select class="form-control" name="category_id" id="category_id" required="true">
-                                <option value="">-- Chọn --</option>
-                                <?php
-                                    foreach($categoryItems as $category) {
-                                        if($category['id'] == $category_id) {
-                                            echo '<option selected value="'.$category['id'].'">'.$category['name'].'</option>';
-                                        } else {
-                                            echo '<option value="'.$category['id'].'">'.$category['name'].'</option>';
+                        <div class="left_div">
+                            <div class="form_group" style="top: 5%">
+                                <label for="usr">Tên sản phẩm:</label>
+                                <input required="true" type="text" class="form-control" id="usr" name="title" value="<?=$title?>">
+                                <input type="text" name="id" value="<?=$id?>" hidden="true">
+                            </div>
+                            
+                            <div class="form_group" style="top: 20%">
+                                <label for="usr">Loại Sản Phẩm:</label>
+                                <select class="form-control" name="category_id" id="category_id" required="true">
+                                    <option value="">-- Chọn --</option>
+                                    <?php
+                                        foreach($categoryItems as $category) {
+                                            if($category['id'] == $category_id) {
+                                                echo '<option selected value="'.$category['id'].'">'.$category['name'].'</option>';
+                                            } else {
+                                                echo '<option value="'.$category['id'].'">'.$category['name'].'</option>';
+                                            }
                                         }
-                                    }
-                                ?>
-                            </select>
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="form_group" style="top: 35%">
+                                <label for="description">Mô tả sản phẩm:</label>
+                                <input required="true" type="text" class="form-control" id="description" name="description" value="<?=$description?>">
+                            </div>
+                            
+                            <div class="form_group"style="top: 50%">
+                                <label for="status">Trạng thái:</label>
+                                <select class="form-control" name="status_id" id="status_id" required="true">
+                                    <option value="">-- Chọn --</option>
+                                    <?php
+                                        foreach($statusList as $status) {
+                                            if($status['id'] == $status_id) {
+                                                echo '<option selected value="'.$status['id'].'">'.$status['status'].'</option>';
+                                            } else {
+                                                echo '<option value="'.$status['id'].'">'.$status['status'].'</option>';
+                                            }
+                                        }
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="form_group" style="top: 65%">
+                                <label for="price">Giá:</label>
+                                <input required="true" type="number" class="form-control" id="price" name="price" value="<?=$price?>">
+                            </div>
                         </div>
-                        <div class="form_group" style="top: 25%">
-                            <label for="description">Mô tả sản phẩm:</label>
-                            <input required="true" type="text" class="form-control" id="description" name="description" value="<?=$description?>">
+                        <div class="right_div">
+                            <div class="title_of_img_card">
+                                <label for="thumbnail">Hình ảnh sản phẩm:</label>
+                            </div>
+                            <div class="img_card">
+                                <input type="file" name="image" id="product_img">
+                            </div>
                         </div>
-                        
-                        <div class="form_group"style="top: 35%">
-                            <label for="status">Trạng thái:</label>
-                            <select class="form-control" name="status_id" id="status_id" required="true">
-								<option value="">-- Chọn --</option>
-								<?php
-									foreach($statusList as $status) {
-										if($status['id'] == $status_id) {
-											echo '<option selected value="'.$status['id'].'">'.$status['status'].'</option>';
-										} else {
-											echo '<option value="'.$status['id'].'">'.$status['status'].'</option>';
-										}
-									}
-								?>
-							</select>
-                        </div>
-                        <div class="form_group" style="top: 45%">
-                            <label for="price">Giá:</label>
-                            <input required="true" type="number" class="form-control" id="price" name="price" value="<?=$price?>">
-                        </div>
-                        
-                        <div class="form_group" style="top: 55%">
-                            <label for="weight">Khối lượng:</label>
-                            <input required="true" type="number" class="form-control" id="weight" name="weight" value="<?=$weight?>">
-                        </div>
-                        <div class="form_group" style="top: 65%">
-                            <label for="thumbnail">Hình ảnh sản phẩm:</label>
-                            <input required="true" type="text" class="form-control" id="thumbnail" name="thumbnail" value="<?=$thumbnail?>">
-                        </div>     
-                        <div class="cfbtn_div" style="top: 85%">
+                        <div class="bottom_div">
                             <button class="btn btn-success">Xác Nhận</button>
                         </div>
                     </form>

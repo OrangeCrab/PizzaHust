74c46f837961dea61fba6c844574e17347e7b007
leducanh2410 <57927000+leducanh2410@users.noreply.github.com>
2021-11-06 21:43:45 +0700

    Merge remote-tracking branch 'origin/bySon'

---
+++ AdminSystem/InvoiceManagementPage/demo.js
@@ -0,0 +1,6 @@
+document.getElementById('popup').addEventListener('click', function() {
+    document.querySelector('.background').getElementsByClassName.display= 'flex';
+});
+document.querySelector('.close').addEventListener('click',function() {
+    document.querySelector('.background').style.display = 'none';
+});

27347c0e393fdddaa166e47dcfa9d418776b4872
Son <68985886+OrangeCrab@users.noreply.github.com>
2021-11-23 00:25:39 +0700

    Update and rename homepageDraft.php to homepage.php

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -17,7 +17,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="homepageDraft.css">
+    <link rel="stylesheet" href="homepage.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
 </head>
 

395bdd54efe46e8a0dbd8bfaef4ebf5430387e91
Son <68985886+OrangeCrab@users.noreply.github.com>
2021-11-23 08:02:50 +0700

    Add files via upload

--- database/pizzahust.sql
+++ database/pizzahust.sql
@@ -1,4 +1,4 @@
--- add bảng coupon và cp_user vào database của hoàng (update 22/11)
+
 CREATE TABLE `coupon`(
  `id_cp` int AUTO_INCREMENT PRIMARY KEY,
  `name_cp` varchar(20),  
@@ -103,7 +103,7 @@ CREATE TABLE `voucher`(
   `picture` varchar(100),
   `information` varchar(250),
   `discount` int
-)
+);
 
 ALTER TABLE `product` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 
@@ -128,6 +128,6 @@ ALTER TABLE `cp_user` ADD FOREIGN KEY (`cp_id`) REFERENCES `coupon` (`id_cp`);
 INSERT INTO `category` (`id`, `title`) VALUES (NULL, 'Pizza'), (NULL, 'Gà BBQ');
 INSERT INTO `size` (`id`, `name`) VALUES (NULL, 'S'), (NULL, 'M');
 INSERT INTO `status_product` (`id`, `status`) VALUES (NULL, 'Còn hàng'), (NULL, 'Hết hàng');
-INSERT INTO `product` (`id`, `category_id`, `name`, `price`, `status_product_id`, `image`, `description`, `size_id`) VALUES (NULL, '1', 'Pizza Thập Cẩm', '99000', '1', '../../masterial/image/thuc_don/thap_cam.jpg', 'ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon', '1'), (NULL, '1', 'Pizzaminsea', '100000', '1', '../../masterial/image/thuc_don/pizzaminsea.jpg', 'ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon', '1');
-INSERT INTO `product` (`id`, `category_id`, `name`, `price`, `status_product_id`, `image`, `description`, `size_id`) VALUES (NULL, '1', 'Pizza Rau Củ', '120000', '1', '../../masterial/image/thuc_don/rau_cu.jpg', 'Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon ', '2'), (NULL, '1', 'Pizza 4 Vị', '150000', '1', '../../masterial/image/thuc_don/4_vi.jpg', 'Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon ', '2'), 
-            (NULL, '1', 'Pizza Thịt Ngập Mõm', '110000', NULL, '../../masterial/image/thuc_don/5_loai_thit.jpg', 'Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon ', '1'), (NULL, '2', 'Đùi Gà Con', '60000', '1', '../../masterial/image/thuc_don/thap_cam.jpg', 'Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon ', '2');
+INSERT INTO `product` (`id`, `category_id`, `name`, `price`, `status_product_id`, `image`, `description`, `size_id`) VALUES (NULL, '1', 'Pizza Thập Cẩm', '99000', '1', 'thap_cam.jpg', 'ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon', '1'), (NULL, '1', 'Pizzaminsea', '100000', '1', 'pizzaminsea.jpg', 'ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon ngon', '1');
+INSERT INTO `product` (`id`, `category_id`, `name`, `price`, `status_product_id`, `image`, `description`, `size_id`) VALUES (NULL, '1', 'Pizza Rau Củ', '120000', '1', 'rau_cu.jpg', 'Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon ', '2'), (NULL, '1', 'Pizza 4 Vị', '150000', '1', '4_vi.jpg', 'Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon ', '2'), 
+            (NULL, '1', 'Pizza Thịt Ngập Mõm', '110000', NULL, '5_loai_thit.jpg', 'Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon ', '1'), (NULL, '2', 'Đùi Gà Con', '60000', '1', 'thap_cam.jpg', 'Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon Ngon ngon ', '2');

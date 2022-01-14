fcbb084e68cd672ec2bd5746a2e130e86cdd920c
lecuong35 <92246385+lecuong35@users.noreply.github.com>
2022-01-14 09:05:40 +0700

    Add files via upload

--- ShowForGuest/forget/forget.php
+++ ShowForGuest/forget/forget.php
@@ -2,54 +2,45 @@
  require_once('../../database/define.php');
  require_once('../../database/dbhelper.php');
  require_once('../../database/utility.php');
- require_once("process_forget.php");
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta charset="UTF-8">
         <title>Quên mật khẩu</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" >
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
         <link rel="stylesheet" href="forget.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+    
     </head>
     <body>
         <img class="background_img" src="../../masterial/image/bgrAdminPage/bgrLogin.jpg" alt="">
         <div class="screen"></div>
         
-        <a href="../homepage/homepage.php"><img class="logo" src="../../masterial/image/iconHomePage/PizzaHustLogo.svg" alt="logo"></a>
+        <div class="content"> 
+          <a href="../homepage/homepage.php"><img class="logo" src="../../masterial/image/iconHomePage/PizzaHustLogo.svg" alt="logo"></a>
         
-
-
-        <form class="box" action="" method="post">
-            <h2 class="container h2" style="color: orange ;">MẬT KHẨU SẼ ĐƯỢC GỬI VÀO GMAIL BẠN ĐÃ ĐĂNG KÝ TRƯỚC ĐÂY</h2>
-            <p style="color: blue;">Vui lòng nhập gmail bạn đã đăng ký trước đây để lấy lại mật khẩu.</p>
-            <label for="username1">User name</label>
-            <input type="text" name="username" id ="username1" placeholder="pizzahust" required = "true" autofocus>       
-            <p id="username-inform" ></p>
-            <label for="username">Gmail</label>
-            <input type="text" name="email" id ="email" placeholder="abc123@gmail.com" required = "true" onblur="gmail()">       
-            <p id="gmail-inform" ></p>
-            <input type="submit" name="" id="submit" value="GỬI" >
-           
-            
-        </form>
+          <form class="box" action="" method="post">
+              <h2 class="container h2" >MẬT KHẨU SẼ ĐƯỢC GỬI VÀO EMAIL BẠN ĐÃ ĐĂNG KÝ TRƯỚC ĐÂY</h2>
+              <!-- <h4 class="container h4">Vui lòng nhập email bạn đã đăng ký trước đây để lấy lại mật khẩu.</h4> -->
+              <label for="username" class="label_for_email">Email</label>
+              <input type="text" name="email" id ="email" placeholder="abc123@gmail.com" required = "true" onblur="gmail()" autofocus>       
+              <p id="gmail-inform" ></p>
+              <input type="submit" name="" id="submit" value="GỬI" >           
+          </form>
+        </div>
         <script>
-
-            var gmail_inform = document.getElementById("gmail-inform");
             function gmail(){
-                var gmail = document.getElementById("username").value;
+                var gmail_inform = document.getElementById("gmail-inform");
+                var gmail = document.getElementById("email").value;
                 const regex = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
                 var check = regex.test(gmail);
 
                 if(!check){
-                   gmail_inform.textContent = "trường này phải là gmail !";
-                   gmail_inform.style.color = "red";
-                   gmail_inform.style.fontSize = "small";
+                   gmail_inform.textContent = "trường này phải là email !";
                    document.getElementById('submit').disabled = true;
                 }
                 else{
@@ -58,11 +49,7 @@
                 }
             }
 
-            // function click(){
-            //     gmail_inform.addEventListener('click', function (){
-            //         gmail_inform.innerHTML = null ;
-            //     });
-            // }
+           
              
            
 
@@ -70,6 +57,9 @@
        
     </body>
 </html>
+<?php
+ require_once("process_forget.php");
+?>
 
 
 
--- ShowForGuest/forget/process_forget.php
+++ ShowForGuest/forget/process_forget.php
@@ -2,70 +2,58 @@
     require '../forget/Exception.php';
     require '../forget/PHPMailer.php';
     require '../forget/SMTP.php';
+    session_start();
 
     use PHPMailer\PHPMailer\Exception;
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\SMTP;
-
-    if(!empty($_POST)){
-        $username = getPost('username');
-       $sql = "select username from user_account where username = '$username'";
-       $check_username = executeResult($sql);
-       if(!empty($check_username)){
-           $email = getPost('email');
-           $sql_email = "select email from user_account where email = '$email' and username = '$username'";
-           $check_email = executeResult($sql_email);
-           if(!empty($check_email)){
-              // check password
-              $sql_pass = "select password from user_account where email = '$email'";
-              $password = array(executeResult($sql_pass));
-              $pass = $password[0][0]['password'];
+    
+    $email = getPost('email');
+    if(!empty($email)){
+        $sql = "select email from user_account where email = '$email'";
+        $check_email = array(executeResult($sql));
+        if($check_email[0][0]['email'] != null){
+        // check password
+        $sql_pass = "select password from user_account where email = '$email'";
+        $password = array(executeResult($sql_pass));
+        $pass = $password[0][0]['password'];
             
 
-               // gui mk
-               $mail = new PHPMailer();
+        // gui mk
+        $mail = new PHPMailer();
 
-               $mail->isSMTP();
-               $mail->Host = "smtp.gmail.com";
-               $mail->SMTPAuth = "true";
-               $mail->SMTPSecure = "ssl";
-               $mail->Port = "465";
-               $mail->Username = "pizzahust123@gmail.com";
-               $mail->Password = "cnpmnhom6";
-               $mail->Subject = "your password of PizzaHust account";
-               $mail->setFrom("pizzahust123@gmail.com");
-               $mail->Body = "Mật khẩu : $pass";
-               $mail->addAddress($email);
-               if(!$mail->send()){
-                  echo '
-                      <script>
-                           alert("Please, turn off security of your gmail to accept our mail !");
-                        </script>
-                    ';
-                }
-                else{
-                    header("location: ../login/login_user.php");
-                    die();
-                }
-                $mail->smtpClose();                        
-            }
-            else{
-               echo '
-                   <script>
-                        alert("Email không đúng !");
-                   </script>
-                ';
-            }
-
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = "true";
+        $mail->SMTPSecure = "ssl";
+        $mail->Port = "465";
+        $mail->Username = "pizzahust123@gmail.com";
+        $mail->Password = "cnpmnhom6";
+        $mail->Subject = "your password of PizzaHust account";
+        $mail->setFrom("pizzahust123@gmail.com");
+        $mail->Body = "Mật khẩu : $pass";
+        $mail->addAddress($email);
+        if(!$mail->send()){
+            echo '
+                <script>
+                    window.alert("Please, turn off security of your gmail to accept our mail !");
+                </script>
+            ';
         }
-       else {
-           echo '
-           <script type="text/javascript">
-               alert("Tên người dùng không tồn tại !"); 
-            </script>
-               
-           ';
-       }
-        
+        else{
+            $_SESSION['password-changed'] = $email;
+            $_SESSION['count_for_changepassword'] = 0;
+            header("location: ../login/login_user.php");
+        }
+        $mail->smtpClose();                        
     }
+    else{
+        echo '
+            <script>
+                 window.alert("Email không đúng !");
+            </script>
+        ';
+    }
+    }
+    
 ?>

45d945f80c8f2d3555210a3c060c4fa060eadbe6
lecuong35 <92246385+lecuong35@users.noreply.github.com>
2022-01-16 16:59:46 +0700

    Add files via upload

--- AdminSystem/login_form.php
+++ AdminSystem/login_form.php
@@ -18,11 +18,32 @@ require_once ('process_login.php');
         <a href="../ShowForGuest/homepage/homepage.php"><img class="logo" src="../masterial/image/iconHomePage/PizzaHustLogo.svg" alt="logo"></a>
         
         <form class="box" action="" method="post">
-            <h2>LOGIN</h2>
-            <input type="text" name="username" id ="username" placeholder="Username">       
-            <input type="password" name="password" id ="password" placeholder="Password">
-            <input type="submit" name="" value="Login">
+            <h2>ĐĂNG NHẬP</h2>
+            <input type="text" name="username" id ="username" placeholder="Username"  required="true" autofocus>  
+
+            <input type="password" name="password" id ="password" placeholder="Password" onblur="password_check()">
+            <p id="password_check"></p>
+            <input type="submit" id="submit" name="" value="Đăng nhập">
         </form>
+        <script>
+            function password_check(){
+                var password = document.getElementById("password").value;
+                var password_inform = document.getElementById("password_check");
+                var submit = document.getElementById("submit");
+                var size = password.length;
+               if(size < 6){
+                    password_inform.textContent = "Mật khẩu phải có ít nhất 6 ký tự !";
+                    submit.disabled = true;
+                }
+                else{
+                    submit.disabled = false;
+                    password_inform.textContent = "";
+                }
+                
+            }
+        </script>
+
+
     </body>
 
 </html>
--- AdminSystem/process_login.php
+++ AdminSystem/process_login.php
@@ -1,8 +1,6 @@
 <?php 
 session_start();
 $_SESSION['admin_id'] = 0;
-require_once('../database/dbhelper.php');
-require_once('../database/utility.php');
 $password = $user_name= $msg = '';
     // kiem tra xem nguoi dung co nhap du lieu khong
     // bằng cách kiem tra xem co qua trinh POST len server khong
@@ -13,21 +11,25 @@ $password = $user_name= $msg = '';
         $password = getPost('password');
         
         // chay cau lenh sql de lay ra gia tri trong database co gia tri bang gia tri nguoi dung nhap vao
-        $sql = "select * from admin";
-        $userExist = getArrResult($sql);
+        $sql_username  = "select username from admin";
+        $sql_password = "select password from admin";
+
+        $check_username = array(executeResult($sql_username));
+        $check_password = array(executeResult($sql_password));
+       
 
 
         // neu khong co nguoi dung nao
-        if($userExist == null){
+        if(empty($check_password) || empty($check_username)){
             $msg = "Đăng nhập không thành công, vui lòng kiểm tra lại tên đăng nhập hoặc mật khẩu";
            // dang nhap khong thanh cong 
         }
         else{
             // dang nhap thanh cong
             //echo"thành công";
-            if ($user_name == $userExist['username'] && $password == $userExist['password']) {
-                $_SESSION['admin_id'] = $userExist['id'];
-                header('location: ./DashBoard/dashboard.php');
+            if ($user_name ==  $check_username[0][0]['username'] && $password == $check_password[0][0]['password']) {
+                $_SESSION['admin_id'] = 1;
+                header("location: DashBoard/dashboard.php");
                 die();
             }else{
                 $msg = "Đăng nhập không thành công, vui lòng kiểm tra lại tên đăng nhập hoặc mật khẩu";
